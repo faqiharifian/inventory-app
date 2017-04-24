8055542b15ceec5003289075bb6593f800b48cb5
Faqih Arifian <faqiharifianaji@gmail.com>
2017-04-24 22:29:34 +0700

    simple layout done

--- app/src/main/java/com/arifian/udacity/inventoryapp/MainActivity.java
+++ app/src/main/java/com/arifian/udacity/inventoryapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.arifian.udacity.inventoryapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        findViewById(R.id.fab).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, EditActivity.class));
+            }
+        });
     }
 }
